7851227ffa3c11875055ad7eeb21d8654249e698
Katherine Gardner <katiegardner@Katherines-MacBook-Air.local>
2021-07-29 11:17:21 -0700

    added endpoint get

--- server/index.js
+++ server/index.js
@@ -6,10 +6,13 @@ const path = require ('path');
 
 const app = express();
 
+app.get('/', (req,res) => {
+    res.sendFile(path.join(__dirname, '../index.html'))
+})
+
 const port = process.env.PORT || 5500
 
-app.listen( port, () => { console.log('Listening on port, ${ port }')
-})
+app.listen(port, () => { console.log(`Listening on port, ${port}`)})
 
 
 
